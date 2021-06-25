2bb321e4b15d7bd95b2dc67af9a21bc0dfe65ead
dotnotation <dotinmotionproductions@gmail.com>
2021-06-25 12:57:50 -0400

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Mr. Chocolate", 10, "M", "Brown", "Calm", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Rowdy", 3, "M", "Black", "Manic", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Tabitha", 4, "F", "Red", "Calm", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Sergeant Brown", 20, "F", "Brown", "Manic", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Melissa", 18, "F", "Blonde", "Playful", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Grinch", 13, "M", "Green", "Malicious", 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Wendy", 8, "F", "White", "Playful", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, 16, "M", "White", "Malicious", 0);
